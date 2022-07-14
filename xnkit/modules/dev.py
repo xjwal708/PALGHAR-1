a95a59ee872f5b9927f74880334b7c0ea3d3d0e9
⚝αикιт кυмαя⚝ <xnkitkumar@gmail.com>
2022-07-14 22:20:33 +0530

    Update dev.py

--- xnkit/modules/dev.py
+++ xnkit/modules/dev.py
@@ -14,7 +14,7 @@ app.CMD_HELP.update(
         "dev": (
             "dev",
             {
-                "eval print("ʟᴇɢᴇɴᴅ'ꜱ ꜱᴀʏ'ꜱ ᴛᴏ <\> ᴀɴᴋɪᴛ 🇮🇳 <\>")": "A nice tool to test python codes.",
+                "eval print("LEGEND'S SAY'S TO ANKIT 🇮🇳")": "A nice tool to test python codes.",
                 "term pip3 install colorama": "Run commands in shell.",
             },
         )
