9077d227b589ed20927649cab70e6ad49210e7b0
⚝αикιт кυмαя⚝ <xnkitkumar@gmail.com>
2022-07-14 13:04:12 +0530

    Create variables.py

---
+++ xnkit/helpers/containers/variables.py
@@ -0,0 +1,14 @@
+class Variables(object):
+    # lower
+    message_ids = {}
+
+    # assistant
+    assistant_name = "XnKiT"
+    assistant_age = "1"
+    assistant_gender = "Male"
+
+    # userbot
+    userbot_name = "XnKiT"
+
+    # photo
+    PIC = "https://telegra.ph/file/474443533bbbc2971f45c.jpg"
