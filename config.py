465d4f54b8bff3f4c1ab8766debed1673f3afc3c
xjwal708 <111631843+xjwal708@users.noreply.github.com>
2022-08-24 12:46:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class Config(object):
     # temporary download location (required)
     TEMP_DICT = os.getenv("TEMP_DICT", os.path.abspath(".") + "/downloads/")
     # official repo for updates
-    UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/XnKiT/XnKiT-UB.git")
+    UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/XJWAL708/PALGHAR-1.git")
     # ------------------
     # heroku api key (required -> if hosted on heroku)
     HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
